d45ed8593b917bbc204a68fbf7f86efad2eb244c
boa50 <bruno.albuquerque50@gmail.com>
2023-05-18 15:02:02 +0930

    Added some code to define specific attributes to the chart

--- part1.R
+++ part1.R
@@ -1,6 +1,19 @@
 if (!require(remotes)) install.packages("remotes")
 remotes::install_github("cpsievert/plotly_book")
 
+library(plotly)
+
 data("diamonds")
 
+# Sample chart
 plot_ly(diamonds, x = ~cut, color = ~clarity, colors = "Accent")
+
+# Changing to specific values
+diamonds %>%
+  plot_ly(
+    x = ~cut,
+    color = I("red"),
+    stroke = I("black"),
+    span = I(3)
+  ) %>%
+  layout(title = "My chart title")
