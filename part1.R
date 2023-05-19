1fc64ecfb2eddc6eb6a4eca971fc8affe0c03fa4
boa50 <bruno.albuquerque50@gmail.com>
2023-05-20 05:08:40 +0930

    Added code that shows how to manipulate plotly charts at a lower level

--- part1.R
+++ part1.R
@@ -1,5 +1,5 @@
-if (!require(remotes)) install.packages("remotes")
-remotes::install_github("cpsievert/plotly_book")
+# if (!require(remotes)) install.packages("remotes")
+# remotes::install_github("cpsievert/plotly_book")
 
 library(plotly)
 library(listviewer)
@@ -19,7 +19,25 @@ diamonds %>%
   ) %>%
   layout(title = "My chart title")
 
-# Debuging created chart
+# Debugging created chart
 p <- plot_ly(diamonds, x = ~cut, color = ~clarity, colors = "Accent")
 plotly_build(p)
 plotly_json(p)
+
+# Manually defining Plotly attributes
+set.seed(99)
+plot_ly() %>%
+  add_trace(
+    type = "scatter",
+    mode = "markers+lines+text",
+    x = 4:6,
+    y = 4:6,
+    text = replicate(3, praise::praise("You are ${adjective}! 🙌")),
+    textposition = "right",
+    hoverinfo = "text",
+    textfont = list(family = "Roboto Condensed", size = 16)
+  ) %>%
+  layout(xaxis = list(range = c(3, 8)))
+
+# Documentation of plotly traces
+schema()
